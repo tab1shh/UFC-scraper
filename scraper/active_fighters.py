4173f132cd3128905fb72ea76467f09d6511c065
Tabish <tabishghouri3@gmail.com>
2025-01-29 17:45:19 -0500

    fixed a bit on ufc rankings file, cleaned up structure a bit, still messy but its enough for now.

--- scraper/active_fighters.py
+++ scraper/active_fighters.py
@@ -1,27 +1,25 @@
-'''
-scrape the names of the fighters on the rankings
-'''
-
 import requests 
-import bs4
 import csv
+from bs4 import BeautifulSoup
 
-url = 'https://www.ufc.com/rankings'
+url = 'https://www.pointspreads.com/ufc/fighters/'
 
 response = requests.get(url)
 
-soup = bs4.BeautifulSoup(response.text, 'lxml')
+soup = BeautifulSoup(response.content, 'html.parser')
 
-name_elements = soup.find_all('td', class_='views-field views-field-title')
+first_names = soup.find_all('div', class_='fighter_first_name')
+last_names = soup.find_all('div', class_='fighter_last_name')
 
-
-with open('ufc_rankings.csv', mode='w', newline='', encoding='utf-8') as file:
+with open('active_fighters.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     # Write the header
-    writer.writerow(['Rank', 'Name'])
+    writer.writerow(['First Name', 'Last Name'])
 
-    for index, element in enumerate(name_elements, start=1):
-        name = element.a.text.strip()  # get the text inside the <a> tag
-        writer.writerow([index, name])  # write rank and name to the CSV
+    # Extract and write the first and last names
+    for first_name, last_name in zip(first_names, last_names):
+        first_name_text = first_name.text.strip()
+        last_name_text = last_name.text.strip()
+        writer.writerow([first_name_text, last_name_text])
 
-print("Names have been saved to 'ufc_rankings.csv'.")
+print("Names have been saved to 'fighter_names.csv'.")
---
+++ scraper/ranked_fighters.py
@@ -0,0 +1,29 @@
+'''
+scrape the names of the fighters on the rankings
+'''
+
+import requests 
+import bs4
+import csv
+import os
+
+url = 'https://www.ufc.com/rankings'
+file_path = os.path.join(os.getcwd(), 'scraper/scraped_files')  
+
+response = requests.get(url)
+
+soup = bs4.BeautifulSoup(response.text, 'lxml')
+
+name_elements = soup.find_all('td', class_='views-field views-field-title')
+
+
+with open(file_path + '/' + 'ufc_rankings.csv', mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    # Write the header
+    writer.writerow(['Rank', 'Name'])
+
+    for index, element in enumerate(name_elements, start=1):
+        name = element.a.text.strip()  # get the text inside the <a> tag
+        writer.writerow([index, name])  # write rank and name to the CSV
+
+print("Names have been saved to 'ufc_rankings.csv'.")
