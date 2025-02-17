525a185e6e4fb84b6bd22ec7e3db771307bab325
tab1shh <164711380+tab1shh@users.noreply.github.com>
2025-02-17 02:35:49 -0500

    still need to figure out a way to scrape and update fighters

--- scraper/main.py
+++ scraper/main.py
@@ -1,13 +1,13 @@
-import get_urls, fights, fighters, fight_stats, fighter_stats, test
+import get_urls, fight_data as fight_data, fighter_data as fighter_data, fight_stats, fighter_stats, test
 def main():
 
     # scrapes all urls from ufcstats.com
-    # get_urls.get_event_urls()
-    # get_urls.get_fight_urls()
-    # get_urls.get_fighter_urls()
+    get_urls.get_event_urls()
+    get_urls.get_fight_urls()
+    get_urls.get_fighter_urls()
 
-    # fights.scrape_all_fights()
-    fighters.scrape_all_fighters()
+    fight_data.scrape_all_fights()
+    fighter_data.scrape_all_fighters()
     fight_stats.scrape_fightstats()
     fighter_stats.scrape_fighterstats()
 
